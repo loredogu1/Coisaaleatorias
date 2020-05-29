a5a3da21af0e4deeee7a323ecf7a72b98d0dc2de
Igor Guilherme <49598006+IgorLoredo@users.noreply.github.com>
2020-05-29 11:08:51 -0300

    up
    nada pronto

---
+++ comparar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int agrc, const char * agrc[]){
+    int lin, col;
+    int dff = 0;
+    FILE *arquivo1();
+    FILE *arquivo2();
+    arquivo1= fogen ("aq1.txt", "r");
+    arquivo2= fogen ("aq2.txt", "r"):
+    printf("aquivo criado")
+    if(arquivo1 == NULL || arquivo2 == NULL){
+        printf("erro nos arquivos");
+        return EXIT_FAILURE;
+    }
+    dff = compare_files (arquivo1, arquivo2, &lin, &col);
+    if(diff == a){
+        printf("os arquivos são iguais");
+    }
+    else{
+        printf("Arquivos não são iguais")
+    }
+    return 0;
+}
+int compare_files [FILE * arquivo1, int *lin, int *col]{
+    char ch1, ch2;
+    *lin = 1
+    *col = 0;
+    do {
+        ch1 = 
+    }
+}
