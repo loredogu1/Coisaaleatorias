48c3eb028d5276cb3bc79501479240dd6cfb691b
Igor Guilherme <49598006+IgorLoredo@users.noreply.github.com>
2020-06-02 11:03:47 -0300

    up

--- reconhcer.c
+++ reconhcer.c
@@ -1,6 +1,11 @@
-#include<s>
-#include<>
+//Guilherme Pereira Loredo - USP Campus Sao Carlos 02/06/2020
+//ler um documento com a data, nome do usuario e qual tipo de maquina ele entrou
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//ler um documento com a data, nome do usuario e qual tipo de maquina ele entrou
 int main(){
-
+FIlE*dio
+printf("");
 
 }
