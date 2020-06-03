a7b91fb07e489c54835b0edf067ebc16c09ed2f3
Igor Guilherme <49598006+IgorLoredo@users.noreply.github.com>
2020-06-02 23:19:49 -0300

    up

--- pow.c
+++ pow.c
@@ -1,6 +1,25 @@
 //Guilherme Pereira Loredo - USP Campus Sao Carlos 02/06/2020
 //funcao para ler o comando pow do trabalha naval
+
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFFER_SIZE 512#
+
+
+int main(){
+    char pow1[]= {'P','O','w'};
+    char pow[];
+
+    int l1;
+
+    scanf("&d, &d",&pow,  );
+    comandopow(pow);
+    return 0;
+}
+    void comandopow(){
+    /*for (int y; yi = 0; y < 6; y++)
+    {
+        pow1[3]= pow[3]
+    }*/
+    if(strcmpa())
+}
